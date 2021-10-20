289cd9e0f62c6bf246d75d19c9bb77774e5b20d9
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2021-10-20 18:40:01 -0400

    Mobile Update

--- main.js
+++ main.js
@@ -10,3 +10,13 @@ $("#lessMedia").click(function(){
         $(".hide").hide(250);
     });
 });
+
+// Mobile script
+if (window.matchMedia('(max-width: 600px)').matches)
+{
+    // do functionality on screens smaller than 768px
+	$(".mobileHide").each(function(){
+        $(".mobileHide").hide(250);
+    });
+	$(".mobileShow").show(250);
+}
