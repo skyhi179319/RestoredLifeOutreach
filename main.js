3ca3a5cb8dabf0bf94f7cf8f62fc3296916aed97
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2021-10-20 21:54:50 -0400

    updated button text through JQuery

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-$("#moreMedia").click(function(){
-	console.log("Button Working");
-    $(".hide").each(function(){
-        $(".hide").show(250);
-    });
-});
-$("#lessMedia").click(function(){
-	console.log("Button Working");
-    $(".hide").each(function(){
-        $(".hide").hide(250);
-    });
-});
-
 // Mobile script
 if (window.matchMedia('(max-width: 600px)').matches)
 {
-    // do functionality on screens smaller than 768px
+	$("#moreMedia").html("All Media");
+	$("#moreMedia").click(function(){
+		console.log("Button Working");
+		$(".hide").each(function(){
+			$(".hide").show(250);
+		});
+	});
+	$("#lessMedia").click(function(){
+		console.log("Button Working");
+		$(".hide").each(function(){
+			$(".hide").hide(250);
+		});
+	});
+
 	$(".mobileHide").each(function(){
         $(".mobileHide").hide(250);
     });
 	$(".mobileShow").show(250);
+}
+else{
+	$("#moreMedia").click(function(){
+		console.log("Button Working");
+		$(".hide").each(function(){
+			$(".hide").show(250);
+		});
+	});
+	$("#lessMedia").click(function(){
+		console.log("Button Working");
+		$(".hide").each(function(){
+			$(".hide").hide(250);
+		});
+	});
 }
