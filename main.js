16bcd5d1702849f5c2f5f5ec4610f8b0af3f528e
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2022-03-05 21:57:29 -0700

    Updated file for  media page

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var service = new Vue({
   el: '#app',
   data: {
   	text: "Join us for worship and God's word every Saturday at 6pm",
-    info: ""
+    info: "No service on 3/5/22"
   }
 });
 
@@ -59,7 +59,113 @@ var googleEle = new Vue({
       }
    }
 });
+// Media Page
+var mobileSet = new Vue({
+  el: '#mobile-set',
+  components:{
+      'mobile-set': {
+        template: `
+            <div>
+                 <div class="column mobileShow mobileColumn" style = "display: none;">
+					<img src="/files/theme/images/1.png">
+					<img src="/files/theme/images/8.jpeg">
+					<img src="/files/theme/images/12.jpeg">
+					<img src="/files/theme/images/7.jpeg">
+					<img src="/files/theme/images/6.jpeg">
+					<img src="/files/theme/images/9.jpeg">
+					<img class="hide one" src="/files/theme/images/13.jpg">
+					<img class="hide" src="/files/theme/images/5.png">
+					<img class="hide" src="/files/theme/images/4.jpg">
+					<img class="hide" src="/files/theme/images/3.png">
+					<img class="hide" src="/files/theme/images/2.jpeg">
+					<img class="hide" src="/files/theme/images/10.jpeg">
+					<img class="hide" src="/files/theme/images/11.jpeg">
+					<img class="hide" src="/files/theme/images/14.png">
+					<img class="hide" src="/files/theme/images/15.png">
+					<video class="hide one" width="320" height="240" controls>
+					<source src="/files/theme/videos/video-1634529419.mp4" type="video/mp4">
+					</video>
+					<video class="hide one" width="320" height="240" controls>
+					<source src="/files/theme/videos/2.mp4" type="video/mp4">
+					</video>
+					<video class="hide" width="320" height="240" controls>
+					<source src="/files/theme/videos/3.mp4" type="video/mp4">
+					</video>
+				  </div>
+            </div>
+          `
+      }
+  }
+})
 
+var photoset1 = new Vue({
+  el: '#photo-set-one',
+  components:{
+      'photo-one': {
+        template: `
+            <div>
+                <img src="/files/theme/images/5.png">
+                <img src="/files/theme/images/4.jpg">
+                <img src="/files/theme/images/3.png">
+                <img src="/files/theme/images/2.jpeg">
+                <img src="/files/theme/images/10.jpeg">
+                <img src="/files/theme/images/11.jpeg">
+                <img src="/files/theme/images/14.png">
+                <img class="hide" src="/files/theme/images/15.png">
+            </div>
+          `
+      }
+  }
+})
+var photoset2 = new Vue({
+  el: '#photo-set-two',
+  components:{
+      'photo-two': {
+        template: `
+            <div>
+                <img src="/files/theme/images/1.png">
+                <img src="/files/theme/images/8.jpeg">
+                <img src="/files/theme/images/12.jpeg">
+                <img src="/files/theme/images/7.jpeg">
+                <img src="/files/theme/images/6.jpeg">
+                <img src="/files/theme/images/9.jpeg">
+                <img src="/files/theme/images/13.jpg">
+            </div>
+          `
+      }
+  }
+})
+var videoset1 = new Vue({
+  el: '#video-set-one',
+  components:{
+      'video-one': {
+        template: `
+            <div>
+                <video class="hide" width="320" height="240" controls>
+                <source src="/files/theme/videos/3.mp4" type="video/mp4">
+                </video>
+            </div>
+          `
+      }
+  }
+})
+var videoset2 = new Vue({
+  el: '#video-set-two',
+  components:{
+      'video-two': {
+        template: `
+            <div>
+                <video class="hide" width="320" height="240" controls>
+                <source src="/files/theme/videos/video-1634529419.mp4" type="video/mp4">
+                </video>
+                <video class="hide" width="320" height="240" controls>
+                <source src="/files/theme/videos/2.mp4" type="video/mp4">
+                </video>
+            </div>
+          `
+      }
+  }
+})
 // Mobile script
 if (window.matchMedia('(max-width: 600px)').matches)
 {
@@ -102,6 +208,8 @@ var sermonDots = new Vue({
             <span class="dot" onclick="currentSlide(1)"></span>
 			<span class="dot" onclick="currentSlide(2)"></span>
 			<span class="dot" onclick="currentSlide(3)"></span>
+			<span class="dot" onclick="currentSlide(4)"></span>
+			<span class="dot" onclick="currentSlide(5)"></span>
           </div>
           `
       }
@@ -113,14 +221,20 @@ var carousel = new Vue({
       'sermon-carousel': {
         template: `
           <div class="slideshow-container" id="sermon">
+		  <div class="mySlides">
+              <sermon-audio link="" speaker="" title="" date="3/12/22"></sermon-audio> 
+            </div>
+		    <div class="mySlides">
+              <sermon-audio link="https://www.youtube.com/embed/tWzchnT3uvs" speaker="David Patterson" title="Who Says You Can't" date="2/26/22"></sermon-audio> 
+            </div>
+			<div class="mySlides">
+              <sermon-audio link="https://www.youtube.com/embed/jc1jNdequkI" speaker="Kelly Patterson" title="Let Your Yes Be Yes" date="2/19/22"></sermon-audio> 
+            </div>
             <div class="mySlides">
               <sermon-audio link="https://www.youtube.com/embed/cjUCtGVo1Q0" speaker="David Patterson" title="7 Ways to Prepare for Jesus' Return" date="2/12/22"></sermon-audio> 
             </div>
             <div class="mySlides">
               <sermon-audio link="https://www.youtube.com/embed/F6z082bsqDk" speaker="Don Brendtro" title="" date="2/5/22"></sermon-audio> 
-            </div>
-			<div class="mySlides">
-              <sermon-audio link="" speaker="" title="" date="2/19/22"></sermon-audio> 
             </div>
 			<a class="prev" onclick="plusSlides(-1)"><i class="fa-solid fa-arrow-left"></i></a>
             <a class="next" onclick="plusSlides(1)"><i class="fa-solid fa-arrow-right"></i></a>
@@ -142,7 +256,7 @@ var sermons = new Vue({
               <p class="serviceFillFont" style="margin-top: 130px;">
                 Come join us on {{date}} as {{speaker}} will be speaking.
               </p>
-              <p class="servicePostFont" style="font-style: italic;">Service will be posted by 11:59pm on {{date}}</p>
+              <p class="servicePostFont" style="font-style: italic;">Service will be posted by 11:59 on {{date}}</p>
             </div>
 			<div v-if="speaker === ''">
               <p class="servicePostFont" style="font-style: italic;margin-top: 130px">Service will be posted by 11:59pm on {{date}}</p>
