00cf1f1d2b9d8a5340033e9c4ab3528868024fd3
skyhi179319 <39777622+skyhi179319@users.noreply.github.com>
2022-03-06 15:34:27 -0700

    added .sh file

---
+++ downloadFiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd Documents
+cd RestoredLife
+curl -o main.js https://restoredlifeoutreach.weebly.com/files/theme/main.js
+curl -o main.css https://restoredlifeoutreach.weebly.com/files/theme/main.css
+echo "downloaded files"
